21c961729353996aac5225fda3b60e222ecf22bc
LIzxPolyU <zexian.li@connect.polyu.hk>
2025-10-20 15:31:02 +0800

    Update test_state_preparation.m

--- test_state_preparation.m
+++ test_state_preparation.m
@@ -3,13 +3,12 @@ clc ; clear ; close all ;
 addpath('QCLAB');
 addpath("rsp_siable");
 
-logging = true; % no record 
-circuit_sim = true ; 
 
 n = 5;
 N = pow2(n) ; 
 state_complex = randn(N,1) + randn(N,1) .* 1j ; 
 state_complex = state_complex ./ norm(state_complex,2) ; 
+logging = true; % no record 
 [circuit, global_phase, CNOT_count] = state_preparation( state_complex, 1, logging ) ; 
 fprintf("N_state(%d) = %d\n\n ", n, CNOT_count) ; 
 circuit.draw()
